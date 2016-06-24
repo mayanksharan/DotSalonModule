8ea64e64cc2404edad7c7846729a1007737493c0
Mayank Sharan <myselfsharan@gmail.com>
2016-06-24 23:01:54 +0530

    Basic Setup

---
+++ app/src/main/java/com/example/mayank/dotsalonmodule/SectionsPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.example.mayank.dotsalonmodule;
+
+/**
+ * Created by mayank on 24/06/16.
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.widget.ImageView;
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 4 total pages.
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Upcoming";
+            case 1:
+                return "Current";
+            case 2:
+                return "Incomplete";
+            case 3:
+                return "Completed";
+        }
+        return null;
+    }
+}
